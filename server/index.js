3dea2caa2e5cf1512c6b45b2b93862a0f3177755
uptopian <aupadhayay4188@gmail.com>
2024-07-11 09:45:07 -0500

    ..

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import app from './server.mjs';
 
+console.log('Starting server...');
+
 const port = process.env.PORT || 5001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+console.log('After app.listen call');
