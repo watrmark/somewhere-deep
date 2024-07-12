58d2da053cef3811306969f69107e3f211944125
uptopian <aupadhayay4188@gmail.com>
2024-07-11 23:12:34 -0500

    ..

--- src/services/postServices.js
+++ src/services/postServices.js
@@ -3,21 +3,21 @@ import services from "./index.js";
 const { api } = services;
 
 export const fetchPosts = async () => {
-  try {
-    const response = await api.get('/posts');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching posts:', error);
-    throw error;
-  }
+    try {
+        const response = await api.get('/api/posts');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        throw error;
+    }
 };
 
 export const fetchPostContent = async (slug) => {
-  try {
-    const response = await api.get(`/posts/${slug}`);
-    return response.data.content;
-  } catch (error) {
-    console.error(`Error fetching post content for slug ${slug}:`, error);
-    throw error;
-  }
+    try {
+        const response = await api.get(`/api/posts/${slug}`);
+        return response.data.content;
+    } catch (error) {
+        console.error(`Error fetching post content for slug ${slug}:`, error);
+        throw error;
+    }
 };
