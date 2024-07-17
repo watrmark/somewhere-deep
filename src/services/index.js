a5fa2b3b68a056ff5cf4dd254c47e3f438adb63e
uptopian <aupadhayay4188@gmail.com>
2024-07-17 09:31:21 -0500

    anything

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = process.env.NODE_ENV === 'production' 
   ? '/api'  // Use relative path in production
-  : 'https://z5xrvtgto5.execute-api.us-east-2.amazonaws.com/dev';  // Use your actual API Gateway URL
+  : 'https://12oj76tee6.execute-api.us-east-2.amazonaws.com/dev';  // Use your actual API Gateway URL
 
 const api = axios.create({
   baseURL: API_URL,
